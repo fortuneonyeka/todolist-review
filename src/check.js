b651eba0c0190ecb0b4d5e9d8d02fa5641edae18
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-01 22:54:30 +0100

    one chunck og code

---
+++ src/check.js
@@ -0,0 +1,19 @@
+export default function checkToDo(todo) {
+  const todos = JSON.parse(localStorage.getItem('toDoList'));
+  const todoIndex = parseInt(todo.parentNode.querySelector('.index').innerHTML, 10);
+  todos[todoIndex].completed = !todos[todoIndex].completed;
+
+  localStorage.setItem('toDoList', JSON.stringify(todos));
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.getElementById('to_do_list');
+
+  list.addEventListener('change', (e) => {
+    const item = e.target;
+    if (item.classList[0] === 'checkbox-input') {
+      const item2 = item.parentElement;
+      item2.classList.toggle('completed-task');
+      checkToDo(item2);
+    }
+  });
+});
